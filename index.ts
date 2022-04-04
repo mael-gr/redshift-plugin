ba00333d912326864557213ffad519537e90d951
mael-gr <76243117+mael-gr@users.noreply.github.com>
2022-04-04 15:12:25 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -122,7 +122,7 @@ export const setupPlugin: RedshiftPlugin['setupPlugin'] = async (meta) => {
 export async function onEvent(event: PluginEvent, { global }: RedshiftMeta) {
     
     
-    console.log('onEvent', global.eventsNotToIgnore.values())
+    console.log('onEvent',   Array.from(global.eventsNotToIgnore.values()) )
     const {
         event: eventName,
         properties,
